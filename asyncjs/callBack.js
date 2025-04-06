58739a2e2e6df13993c30f9f08495df4f65f8a61
sainivi931 <sainivi931@gmail.com>
2025-04-06 23:56:47 +0530

    callBack & callback hell

---
+++ asyncjs/callBack.js
@@ -0,0 +1,44 @@
+// callback functions
+// console.log("Task Started");
+
+// function asyncTask (cb){
+//     console.log("Task is Running");
+//     setTimeout(cb,0)
+
+// }
+
+// asyncTask ( ()=> console.log(name))
+// console.log("Task is Ended");
+
+// const name = "Vishal"
+
+function asyncTask(cb){
+  setTimeout(() => {
+    cb(null, "This is the data from the server");
+  }, 0);
+}
+
+asyncTask((err, data) => {
+  if (err) {
+    throw err;
+  } else console.log("Data", data);
+});
+
+const name = "Vishal";
+
+function makecallApi(cb){
+    setTimeout(()=>{
+     console.log("This is async task Execution");
+     
+    },0)
+}
+
+makecallApi(()=>{ 
+    makecallApi(()=>{
+        makecallApi(()=>{
+            makecallApi(()=>{
+
+            })
+        })
+    })
+})
